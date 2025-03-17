15ecf7c433ffda93ae7f8d74cd4317ac85d7e3d5
Kalled Abdala <111213048+KalledAbdala@users.noreply.github.com>
2025-03-17 09:57:24 -0300

    Update condicionais.java

--- JAVA e ORIENTACAO/secao5/condicionais.java
+++ JAVA e ORIENTACAO/secao5/condicionais.java
@@ -10,7 +10,34 @@ public class condicionais {
         System.out.println(isFalse);
 
 
-        
 
+        String str1 = "Java";
+        String str2 = new String("Java");
+
+        System.out.println(str1 == str2);
+        System.out.println(str1 == "Java");
+
+        System.out.println(str1.equals(str2));
+        System.out.println(str2.equals(str1));
+        System.out.println(str1.equals("Java"));
+
+        String str3 = "JaVa";
+
+        System.out.println(str1.equalsIgnoreCase(str3));
+        System.out.println(str1.equals(str3));
+
+
+
+        int num = 10;
+
+        if (num > 5){
+            System.out.println("O numero e maior que 5");
+        }
+
+        String texto = "Kalled";
+        if (texto.equals("Kalled")){
+            System.out.println("O texto e: " + texto);
+        }
+        
     }
 }
