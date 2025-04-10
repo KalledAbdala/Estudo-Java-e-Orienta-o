dcc74311ffb74b53207c5c3427f866ad4e37e588
Kalled Abdala <kalledgamer@gmail.com>
2025-04-09 21:11:31 -0300

    att Exercisios secao5

--- JAVA e ORIENTACAO/secao5/Exercicios.java
+++ JAVA e ORIENTACAO/secao5/Exercicios.java
@@ -20,7 +20,7 @@ public class Exercicios {
  
 
 
-        System.out.println("Digite um usuaio:");
+        System.out.println("Digite um usuario:");
         String usuario = scanner.next();
 
         System.out.println("Digite a senha:");
@@ -31,6 +31,66 @@ public class Exercicios {
         } else{
             System.out.println("Acesso negado");
         }
+
+
+
+
+        System.out.println("Insira um numero:");
+        int numero = scanner.nextInt();
+
+        String numeroArmazenado = "";
+
+        if (numero % 2 == 0){
+            numeroArmazenado = "Par";
+        } else{
+            numeroArmazenado = "Impar";
+        }
+
+        System.out.println("Seu numero eh: " + numeroArmazenado);
+
+
+
+
+
+        System.out.println("Digite o número correspondente com o dia da semana:\n" +
+                   "1 - Domingo\n" +
+                   "2 - Segunda\n" +
+                   "3 - Terça\n" +
+                   "4 - Quarta\n" +
+                   "5 - Quinta\n" +
+                   "6 - Sexta\n" +
+                   "7 - Sábado");
+        int diaDaSemana = scanner.nextInt();
+
+        switch (diaDaSemana) {
+            case 1:
+                System.out.println("Domingo nao e dia Util");
+                break;
+            case 2:
+                System.out.println("Segunda e dia Util !!!");
+                break;
+            case 3:
+                System.out.println("Terca e dia Util !!!");
+                break;
+            case 4:
+                System.out.println("Quarta e dia Util !!!");
+                break;
+            case 5:
+                System.out.println("Quinta e dia Util !!!");
+                break;
+            case 6:
+                System.out.println("sexta e dia Util !!!");
+                break;
+            case 7:
+                System.out.println("Sabado nao e dia Util");
+                break;
+            default:
+                System.out.println("Numero invalido");
+                break;
+        }
+        
+
+
 
 
 
